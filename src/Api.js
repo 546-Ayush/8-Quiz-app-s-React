1a061a2342d2187f83eb6349158ac7c2138f2b8e
546-Ayush <ayushsoni546.com>
2022-09-04 16:50:18 +0530

    first commit

---
+++ src/Api.js
@@ -0,0 +1,82 @@
+export const data = [
+    {
+        key: 1,
+        question: 'Which of the following is used in React.js to increase performance?',
+        options: [
+            { text: 'Virtual DOM', isCorrect: true },
+            { text: 'Original DOM', isCorrect: false },
+            { text: 'Both A and B', isCorrect: false },
+            { text: 'None of the above', isCorrect: false },
+        ]
+    },
+    {
+        key: 2,
+        question: 'Which of the following option is correct in the case of the Babel?',
+        options: [
+            { text: 'Babel is a Compiler.', isCorrect: false },
+            { text: 'Babel is a Transpilar.', isCorrect: false },
+            { text: 'None of the above.', isCorrect: false },
+            { text: 'Both A and B are correct.', isCorrect: true },
+        ]
+    },
+    {
+        key: 3,
+        question: 'Which of the following is used to pass data to a component from outside in React.js?',
+        options: [
+            { text: 'SetState', isCorrect: false },
+            { text: 'Render with arguments', isCorrect: false },
+            { text: 'Props', isCorrect: true },
+            { text: 'PropTypes', isCorrect: false },
+        ]
+    },
+    {
+        key: 4,
+        question: 'Which of the following function is called to render HTML to the web page in React?',
+        options: [
+            { text: 'rendering()', isCorrect: false },
+            { text: 'ReactDOM_render()', isCorrect: false },
+            { text: 'render()', isCorrect: true },
+            { text: 'none', isCorrect: false },
+        ]
+    },
+    {
+        key: 5,
+        question: 'Which of the following is the correct name of React.js?',
+        options: [
+            { text: 'React', isCorrect: false },
+            { text: 'React.js', isCorrect: false },
+            { text: 'ReactJS', isCorrect: false },
+            { text: 'All of the above', isCorrect: true },
+        ]
+    },
+    {
+        key: 6,
+        question: 'What is the default port where webpack-server runs?',
+        options: [
+            { text: '3830', isCorrect: false },
+            { text: '3000', isCorrect: true },
+            { text: '8300', isCorrect: false },
+            { text: '3001', isCorrect: false },
+        ]
+    },
+    {
+        key: 7,
+        question: 'What are the two ways to handle data in React?',
+        options: [
+            { text: 'Services & Components', isCorrect: false },
+            { text: 'State & Services', isCorrect: false },
+            { text: 'State & Component', isCorrect: false },
+            { text: 'State & Props', isCorrect: true },
+        ]
+    },
+    {
+        key: 8,
+        question: 'Does React.js create a VIRTUAL DOM in the memory?',
+        options: [
+            { text: 'TRUE', isCorrect: true },
+            { text: 'FALSE', isCorrect: false },
+            { text: 'Can be true or false', isCorrect: false },
+            { text: 'Cannot say', isCorrect: false },
+        ]
+    },
+]
--- src/App.js
+++ src/App.js
@@ -1,25 +1,46 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import style from './App.module.css';
+import { data } from './Api';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    let len = data.length - 1;
+    const [count, setCount] = useState(0);
+    const [showResult, setShowResult] = useState(false);
+    const [correctAnswer, setCorrectAnswer] = useState(0)
+
+    function handleOnClick(isCorrect) {
+        if (isCorrect) {
+            alert('Correct answer');
+            setCorrectAnswer(correctAnswer + 1)
+        }
+        if (count < len) {
+            setCount(count + 1);
+        }
+        else {
+            setShowResult(!showResult);
+        }
+    }
+
+    return (
+        <>
+            {showResult ? (<h1 className={style.output}>You have scored {correctAnswer} out of {data.length}</h1>) :
+                (
+                    <div className={style.parent}>
+                        <div className={style.box}>
+                            <div className={style.question}>
+                                <h1>Question {count + 1}/<span>8</span> </h1>
+                                <p>{data[count].question}</p>
+                            </div>
+                            <div className={style.options}>
+                                {data[count].options.map((val) =>
+                                    <button onClick={() => handleOnClick(val.isCorrect)} className={style.opt}>{val.text}</button>
+                                )}
+                            </div>
+                        </div>
+                    </div>
+                )}
+        </>
+    )
 }
 
 export default App;
